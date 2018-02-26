7be81f2dcb336d18365ebc4b01cf14094fdb3bee
AkihiroUzuka <akihiro.uzuka@el-ltd.co.jp>
2018-02-26 17:31:51 +0900

    180226

---
+++ CheckTest_p.128.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	int a = 0, b = 0;
+
+
+	printf("貳つの整数の大小判定\n");
+	printf("壹つ目の整数を入力してください......");
+	scanf("%d", &a);
+	
+
+	printf("貳つ目の整数を入力してください......");
+	scanf("%d", &b);
+
+	if (a > b) { printf("大きいほうの数は、壹つ目の数\"%d\"です", a); }
+	else if (a < b) { printf("大きいほうの数は、貳つ目の数\"%d\"です", b); }
+	else if (a == b) { printf("両者は同じ数です"); }
+
+
+	return 0;
+
+
+}
---
+++ list_5.4_p.139.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	int a = 0;
+	printf("1から10までの奇数を表示します\n");
+
+	for (a = 1; a <= 10; a = a + 2)
+	{
+		printf("%d\n", a);
+	}
+
+	printf("今はもうループから抜けています。変数aの中身は%dです\n", a);
+
+	return 0;
+}
