80a1b6689d5ea1503673422b2ed901fe23cb5cfc
AkihiroUzuka <akihiro.uzuka@el-ltd.co.jp>
2018-03-07 14:48:41 +0900

    180307

---
+++ CheckTest_p.196.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	char a[95];//94+終端文字。
+	
+	for (int i = 33; i <= 126; i = i + 1) {
+
+		a[i-33] = i;
+		
+	}
+	a[94] = 0;//終端文字必要
+
+	for (int j = 0; j <= 94; j++) {
+		printf("%c", a[j]);
+	}
+
+	printf("\n\n");
+
+	printf("%s", a);//この書き方だと配列宣言がintでは上手くいかずcharだと大丈夫。つまり、%sは整数文字コードの配列を直接文字列に変換することはできないっぽい。
+
+	return 0;
+}
+
+
