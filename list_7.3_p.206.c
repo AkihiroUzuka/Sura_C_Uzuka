bb4edef47387dcf756cbeacbb083c21020559533
AkihiroUzuka <akihiro.uzuka@el-ltd.co.jp>
2018-03-08 15:41:50 +0900

    180308

---
+++ list_7.3_p.206.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+void ayamaru(int a) {
+
+	for (int i=1;i<=a;i++)
+	printf("ごめんなさい\n");
+	return;
+}
+
+
+
+int main(void) {
+
+
+
+	printf("岡嶋くん、気持ち悪いからあっちに行ってよ\n");
+	ayamaru(5);
+
+
+	printf("岡嶋くん、また授業中にゲームしていたのかね\n");
+	ayamaru(2);
+
+
+	printf("岡嶋くん、明日の遠足、ペア組むの嫌だから休んでね\n");
+	ayamaru(0);
+
+
+	return 0;
+}
---
+++ list_7.4_p.208.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int ayamaru(int a) {
+
+	for (int i=1;i<=a;i++)
+	printf("ごめんなさい\n");
+	return 10; 
+}
+
+
+
+int main(void) {
+
+	int b = 0;
+
+	printf("岡嶋くん、気持ち悪いからあっちに行ってよ\n");
+	b = ayamaru(5);
+	printf("戻ってきた結果は、%dでした\n\n", b);
+
+	printf("岡嶋くん、また授業中にゲームしていたのかね\n");
+	b = ayamaru(2);
+	printf("戻ってきた結果は、%dでした\n\n", b);
+
+	printf("岡嶋くん、明日の遠足、ペア組むの嫌だから休んでね\n");
+	b = ayamaru(0);
+	printf("戻ってきた結果は、%dでした\n\n", b);
+
+	return 0;
+}
