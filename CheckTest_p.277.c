5f28e8b3f15d666e7b78f771c8e15a922290a821
AkihiroUzuka <akihiro.uzuka@el-ltd.co.jp>
2018-03-16 17:19:40 +0900

    180316

---
+++ CheckTest_p.277.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	float a = 0, b = 0;
+
+	float *pa;
+	float *pb;
+
+	pa = &a;//ポインタpaに変数aのアドレス&aを代入
+	pb = &b;
+
+
+	scanf("%f", pa);//*なしでアドレス
+	scanf("%f", pb);
+
+	printf("%f", *pa + *pb);//*付きで中身
+
+	return 0;
+
+}
---
+++ CheckTest_p.277_2.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	char a = 'A'; //シングルクォーテーション
+	char *pa;
+
+	pa = &a;
+	
+	printf("%p", pa);//show the address
+	printf("%c", *pa);
+
+	return 0;
+}
