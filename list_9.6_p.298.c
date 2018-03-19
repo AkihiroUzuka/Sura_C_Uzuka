55f3994146a3d717b0eb26d166fb87833057bf41
AkihiroUzuka <akihiro.uzuka@el-ltd.co.jp>
2018-03-19 15:16:54 +0900

    180319

---
+++ list_9.6_p.298.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	FILE *f;
+
+	f = fopen("test.txt", "w");
+	fclose(f);
+	printf("ファイルを開いて閉じました\n");
+
+	return 0;
+}
---
+++ list_9.7_p.303.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	FILE *f;
+	f = fopen("test.txt", "r");
+
+	if (f == NULL) {
+		printf("ファイルが開けなかった\n");
+
+		return 1;
+	}
+	else {
+		printf("ファイルが開けたぞ！\n");
+	}
+
+
+	fclose(f);
+	printf("ファイルを閉じました\n");
+
+	return 0;
+}
