64e9fe157717221f5be89c17f5a04dbe02c026f8
AkihiroUzuka <akihiro.uzuka@el-ltd.co.jp>
2018-03-20 16:47:06 +0900

    180320

---
+++ CheckTest_q1_p.316.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	FILE *f; //ファイル構造体へのポインタの作成
+	f = fopen("seiyu.txt", "a");
+
+	if (f == NULL) {
+		printf("ファイルが開けなかった\n");
+		return 1;
+	}
+	else {
+		printf("ファイルが開けたぞ！");
+	}
+
+	struct info {  // infoという名前の構造体（任意の複数の型をまとめた一つの型）作成宣言
+		char name[50];  //memberはname, age and daihyousaku
+		int age;
+		char daihyosaku[100];
+	}seiyu[10];  //info型を持つseiyuという変数を作成
+
+	printf("好きな声優さんの名前、年齢、代表作を入力してください\n");
+
+	for (int i = 0; i < 10; i = i + 1) {
+		scanf("%s %d %s", seiyu[i].name, &seiyu[i].age, seiyu[i].daihyosaku);
+		fprintf(f, "%s %d %s\n", seiyu[i].name, seiyu[i].age, seiyu[i].daihyosaku);
+	}
+
+	fclose(f);
+	printf("ファイルを閉じました\n");
+
+	return 0;
+}
---
+++ CheckTest_q2_p.317.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	FILE *f; //ファイル構造体へのポインタの作成
+	f = fopen("seiyu.txt", "a");
+
+	if (f == NULL) {
+		printf("ファイルが開けなかった\n");
+		return 1;
+	}
+	else {
+		printf("ファイルが開けたぞ！");
+	}
+
+	
+	char name[10][50];  //memberはname, age and daihyousaku
+	int age[10];
+	char daihyosaku[10][100];
+	
+	printf("好きな声優さんの名前、年齢、代表作を入力してください\n");
+
+	for (int i = 0; i < 10; i = i + 1) {
+		scanf("%s %d %s", name[i], &age[1], daihyosaku[i]);
+		fprintf(f, "%s %d %s\n", name[i], age[1], daihyosaku[i]);
+	}
+
+	fclose(f);
+	printf("ファイルを閉じました\n");
+
+	return 0;
+}
